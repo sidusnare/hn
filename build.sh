3859fd264bf7b62c9cdca455e97ee7c5ed69e4a1
Fred Dinkler IV <sidusnare@gmail.com>
2015-01-06 16:20:02 -0500

    updates

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-rm hn
-if gcc hn.c -o hn; then
-	echo "Should sucseed"
+test -e hn && rm hn
+if gcc -O3 hn.c -o hn; then
+	echo -e "\nShould sucseed and return true"
 	./hn google.com 80 5 && echo Returns true || echo Returns false
-	echo "Should timeout"
+
+	echo -e "\nShould timeout and return false"
 	./hn google.com 81 5 && echo Returns true || echo Returns false
-	echo "Should be refused"
+
+	echo -e "\nShould be refused and return false"
 	./hn solprime.net 81 5 && echo Returns true || echo Returns false
-	echo "Should not be able to resolve"
+
+	echo -e "\nShould not be able to resolve and return false"
 	./hn m5NQzdYH5qFhtC9bzb5C5fU7 80 5 && echo Returns true || echo Returns false
 else
 	echo "Unable to compile"
